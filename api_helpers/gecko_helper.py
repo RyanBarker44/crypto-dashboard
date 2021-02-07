13fa424522004f2086bb2d06c923ee8ba179954f
Ryan <ryza165@hotmail.com>
2021-02-08 01:30:35 +1000

    Added a comment as a test push

--- api_helpers/gecko_helper.py
+++ api_helpers/gecko_helper.py
@@ -4,12 +4,14 @@ import requests
 
 
 cg = CoinGeckoAPI()
-
+ 
 
 def get_current_price(ticker):
+	"""Takes a ticker symbol and returns an object containing all coin data"""
 
 	coin_data = cg.get_coin_by_id(ticker)
 
 	localised_current_price = coin_data["market_data"]["current_price"][api_settings.CURRENCY]
 
 	return float(localised_current_price)
+	
